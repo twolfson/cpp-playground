9ecd53b7d46ee96094e08581d7841f40fe2ba50f
Todd Wolfson <todd@twolfson.com>
2017-05-08 01:15:29 -0700

    Completed types info

--- 03-types/main.cpp
+++ 03-types/main.cpp
@@ -16,9 +16,20 @@ int main()
   unsigned foo_unsigned = 500;
 
   // Output info about our variables
+  cout << "Values\n";
   cout << foo_bool << "\n";
   cout << foo_char << "\n";
   cout << foo_int << "\n";
   cout << foo_double << "\n";
+  cout << foo_double_alt_syntax << "\n";
   cout << foo_unsigned << "\n";
+  cout << "\n";
+
+  // Output other info
+  cout << "Sizes\n";
+  cout << sizeof(foo_bool) << "\n";
+  cout << sizeof(foo_char) << "\n";
+  cout << sizeof(foo_int) << "\n";
+  cout << sizeof(foo_double) << "\n";
+  cout << sizeof(foo_unsigned) << "\n";
 }
