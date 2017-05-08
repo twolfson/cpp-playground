70e2173d2f577a0b70a3167fe367b55215870908
Todd Wolfson <todd@twolfson.com>
2017-05-08 14:26:52 -0700

    Completed exploring updating structs via helper function

--- 06-structs/main.cpp
+++ 06-structs/main.cpp
@@ -7,11 +7,26 @@ using namespace std;
 
 // Define a struct
 struct List {
-  int size;
-  double* elem;
+  int size = 0;
+  double* elem = NULL;
 };
 
 // Define helper functions
+void update_struct_by_value(List list)
+{
+  list.size = 3;
+  list.elem = new double[3] {1, 2, 3};
+}
+void update_struct_by_reference(List& list)
+{
+  list.size = 3;
+  list.elem = new double[3] {1, 2, 3};
+}
+void update_struct_by_pointer(List* list_ptr)
+{
+  list_ptr->size = 3;
+  list_ptr->elem = new double[3] {1, 2, 3};
+}
 
 // Define our main function
 int main()
@@ -22,5 +37,22 @@ int main()
   list.elem = new double[2] {20, 50};
 
   // Output info
+  //   2 20 50
   cout << list.size << " " << list.elem[0] << " " << list.elem[1] << "\n";
+
+  // Try out our different updates
+  //   No updates to struct (i.e. 0)
+  List list_by_value;
+  update_struct_by_value(list_by_value);
+  cout << list_by_value.size << "\n";
+
+  //   Updates struct (i.e. 3)
+  List list_by_reference;
+  update_struct_by_reference(list_by_reference);
+  cout << list_by_reference.size << "\n";
+
+  //   Updates struct (i.e. 3), requires different syntax
+  List list_by_pointer;
+  update_struct_by_pointer(&list_by_pointer);
+  cout << list_by_pointer.size << "\n";
 }
