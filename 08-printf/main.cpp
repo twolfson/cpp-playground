8f7e107047dc483cc71d59c408026a72a05e448a
Todd Wolfson <todd@twolfson.com>
2017-05-14 18:28:16 -0700

    Got printf working

---
+++ 08-printf/main.cpp
@@ -0,0 +1,11 @@
+// Load in our dependencies
+// DEV: We use </> instead of quotes due to it being stdlib
+// DEV: We are using `c` prefix due to it being a C library
+#include <cstdio>
+
+// Define our main function
+int main()
+{
+  char name[] = "World";
+  printf("Hello %s\n", name);
+}
