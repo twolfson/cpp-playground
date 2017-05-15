638aeb913cdab619b93e71089567a26fdc3554fc
Todd Wolfson <todd@twolfson.com>
2017-05-14 17:53:47 -0700

    Started on default skeleton for LinkedList

--- 08-linked-list/main.cpp
+++ 08-linked-list/main.cpp
@@ -7,46 +7,35 @@
 
 // Define our classes
 class Node {
+public:
   int val;
   Node* next;
 
   Node(int val, Node* next) {
-    this->val = val
+    this->val = val;
     this->next = next;
   }
   ~Node() {
     // TODO: What is `[]` syntax for?
     // TODO: Try out a unique pointer instead of destructor
-    delete this->val;
     delete this->next;
   }
-}
+};
 
 class LinkedList {
 public:
   Node* head = NULL;
 
   LinkedList() {
-    this->size = size;
-    this->elem = new double[size];
   };
   ~LinkedList() {
-    delete[] this->elem;
+    // TODO: What is `[]` syntax for?
+    // TODO: Try out a unique pointer instead of destructor
+    delete this->head;
   }
 
-  double& get (int index) {
-    if (index < 0 || index >= this->size) {
-      throw out_of_range("Requested index outside of size");
-    }
-    return this->elem[index];
-  };
-  double& operator[] (int index) {
-    return this->get(index);
-  };
-
-  void increment (int index) {
-    double& val_ref = this->get(index);
-    val_ref += 1;
+  void push(int val) {
+    // TODO: Use a template for dynamic node types
   }
 };
 
@@ -55,10 +44,4 @@ int main()
 {
   // Create an example class
   LinkedList list;
-  list[0] = 20;
-  list[1] = 30;
-  cout << list.size << " " << list[0] << " " << list[1] << "\n";
-  list.increment(1);
-  cout << list[1] << "\n";
-  list[20];
 }
