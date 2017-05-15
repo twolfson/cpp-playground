153bb7dbd197ab335f30788280d9bfc34d34be03
Todd Wolfson <todd@twolfson.com>
2017-05-14 18:12:47 -0700

    Wrote some code that should work but references/pointers are throwing me

--- 08-linked-list/main.cpp
+++ 08-linked-list/main.cpp
@@ -2,23 +2,38 @@
 #include <iostream>
 #include <stdexcept>
 
+// Based on
+//   https://github.com/gcc-mirror/gcc/blob/gcc-7_1_0-release/libstdc%2B%2B-v3/include/bits/stl_list.h#L250-L257
+//   http://www.cplusplus.com/reference/list/list/begin/
+
 // TODO: Break up into separate files
 // TODO: Write up tests
 
 // Define our classes
 class Node {
 public:
-  int val;
-  Node* next;
+  int* val;
+  Node* next = NULL;
 
-  Node(int val, Node* next) {
+  Node(int* val, Node* next) {
     this->val = val;
     this->next = next;
   }
   ~Node() {
     // TODO: What is `[]` syntax for?
     // TODO: Try out a unique pointer instead of destructor
+    delete this->val;
     delete this->next;
+  }
+
+  // When we are dereferenced, return our value
+  int operator*() {
+    return *this->val;
+  }
+
+  // When we want to move to the next node, return a point to the next one
+  Node* operator++() {
+    return this->next;
   }
 };
 
@@ -34,8 +49,18 @@ public:
     delete this->head;
   }
 
-  void push(int val) {
+  Node* begin() {
+    return this->head;
+  }
+
+  void push_start(int& val) {
     // TODO: Use a template for dynamic node types
+    if (this->head == NULL) {
+      this->head = val;
+    } else {
+      Node& old_head = this->head;
+      this->head = Node(val, old_head);
+    }
   }
 };
 
@@ -44,4 +69,5 @@ int main()
 {
   // Create an example class
   LinkedList list;
+  std::cout << list.begin();
 }
