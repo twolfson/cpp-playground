623a9929fb33541ae789e1058d71721e4e9f0b33
Todd Wolfson <todd@twolfson.com>
2017-05-14 22:39:26 -0700

    Added note about char* vs char[]

--- 09-arrays/main.cpp
+++ 09-arrays/main.cpp
@@ -22,6 +22,9 @@ int main()
   // DEV: We use `const char` to silence errors about converting strings to `char*`
   //   http://stackoverflow.com/a/16867229
   // DEV: We are generating a 2 dimensional array (i.e. string = array, array of strings = array of arrays)
+  // DEV: Technically `char *` is not preferred for strings
+  //   (i.e. we want `char[]` so we can refer to entirety, not head character)
+  //   but these strings are dynamic length so nested array syntax wouldn't work
   const char* states[STATES_AMOUNT] = {
     "California", "Oregon",
     "Washington", "Texas"
